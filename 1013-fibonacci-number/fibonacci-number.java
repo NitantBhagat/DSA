9b11c9210aba967675dd17bd8cf008afb223e107
Nitant Bhagat <124676972+NitantBhagat@users.noreply.github.com>
2025-07-23 15:52:46 -0400

    Time: 7 ms (46.01%) | Memory: 40.5 MB (37.46%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int fib(int n) {
+        return fibonacci(n);
+    }
+
+    private static int fibonacci(int n){
+        if(n==0){
+            return 0;
+        }else if(n==1|| n==2){
+            return 1;
+        }else{
+            return fibonacci(n-1)+fibonacci(n-2);
+        }
+    }
+}
