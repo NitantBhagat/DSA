01493be1a7c452f6ab51187f5933005c4752906c
Nitant Bhagat <124676972+NitantBhagat@users.noreply.github.com>
2025-08-15 17:44:19 -0400

    Time: 0 ms (100.00%) | Memory: 41.3 MB (29.57%) - LeetSync

--- 100-same-tree/same-tree.java
+++ 100-same-tree/same-tree.java
@@ -19,6 +19,6 @@ class Solution {
             return p==q;
         }
 
-        return (p.val==q.val) && isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+        return (p.val == q.val) && isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
     }
 }
