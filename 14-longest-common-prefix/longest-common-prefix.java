23bd0e6d89dcdff6a5cff788b903efdf085273e2
Nitant Bhagat <124676972+NitantBhagat@users.noreply.github.com>
2025-08-22 11:05:26 -0400

    Time: 1 ms (62.03%) | Memory: 41.6 MB (62.40%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,26 +1,30 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if (strs == null || strs.length == 0) return "";
-        int minLen = Integer.MAX_VALUE;
-        for (String s : strs) minLen = Math.min(minLen, s.length());
-        if (minLen == 0) return "";
+        
+        if(strs == null || strs.length == 0 ) return "";
+        
+        int minLength = Integer.MAX_VALUE;
+        
+        for(String s : strs) minLength = Math.min(minLength, s.length());
+        
+        if(minLength == 0) return "";
 
-        int lo = 0, hi = minLen;
-        while (lo <= hi) {
-            int mid = lo + (hi - lo) / 2;
-            if (isCommonPrefix(strs, mid)) {
-                lo = mid + 1;
-            } else {
-                hi = mid - 1;   
+        int lo =0 , hi = minLength;
+        while(lo<=hi){
+            int mid = lo + (hi-lo) /2;
+            if(isCommonPrefix(strs,mid)){
+                lo = mid +1;
+            }else{
+                hi  = mid -1;
             }
         }
-        return strs[0].substring(0, hi);
+        return strs[0].substring(0,hi);
     }
 
-    private boolean isCommonPrefix(String[] strs, int len) {
-        for (int i = 1; i < strs.length; i++) {
-            for (int j = 0; j < len; j++) {
-                if (strs[i].charAt(j) != strs[0].charAt(j)) return false;
+    private boolean isCommonPrefix(String[] strs, int len){
+        for(int i =1 ; i < strs.length ;i ++){
+            for(int j=0; j < len; j++){
+                if(strs[i].charAt(j) != strs[0].charAt(j)) return false;
             }
         }
         return true;
